f4789bd7bc99f7ac5b9605657bd710a69905166e
Bailey van Antwerpen <99041848@mydavinci.nl>
2017-05-20 16:19:30 +0200

    update

--- model/SpeciesModel.php
+++ model/SpeciesModel.php
@@ -8,3 +8,20 @@ function getAllSpecies() {
 	$db=null;
 	return $query->fetchAll();
 }
+
+function addSpecie() {
+	$species = isset($_POST['species']) ? $_POST['species'] : null;
+
+	if (strlen($species) == 0) {
+		return false;
+	}
+
+	$db = openDatabaseConnection();
+	$sql = "INSERT INTO species(species_description) VALUES :species";
+	$query = $db->prepare($sql);
+	$query->execute(array(
+		':species' => $species
+		));
+
+	return true;
+}
