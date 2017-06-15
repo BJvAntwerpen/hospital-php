d6fecbf70c3f9f70e5c2891417001501187e7cf7
Bailey van Antwerpen <99041848@mydavinci.nl>
2017-06-15 11:37:37 +0200

    update

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -4,8 +4,6 @@ require(ROOT . 'model/HomeModel.php');
 
 function index() {
 	render('home/index', array(
-		'patients' => getAllPatients(),
+		'patients' => getAllPatients()
 		));
 }
-
-/* https://www.w3schools.com/sql/sql_join.asp */
